f8296b42b32007c166626a7622888e72c637600b
Derek(Dongjie)Liu <liudongjie1119@gmail.com>
2025-03-16 00:14:55 -0600

    deliver website

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // 警告会被视为错误，但不会阻止编译
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -18,32 +18,11 @@ const About = () => {
                 I am someone who thrives on staying active, whether it is through my passion for fitness or my love for badminton. 
               </p>
               <p className="text-gray-600 leading-relaxed mt-1">
-              These activities keep me energized and focused, which I channel into my enthusiasm for coding. Writing code has always been an exciting challenge for me—it's a space where I can continuously learn and grow, while solving complex problems. Whether working on full-stack development projects or exploring new technologies, I enjoy every moment of the coding process and love bringing creative ideas to life through my technical skills.
+              These activities keep me energized and focused, which I channel into my enthusiasm for coding. Writing code has always been an exciting challenge for me it is a space where I can continuously learn and grow, while solving complex problems. Whether working on full-stack development projects or exploring new technologies, I enjoy every moment of the coding process and love bringing creative ideas to life through my technical skills.
               </p>
             </div>
             
-            {/* <div>
-              <h3 className="text-2xl font-semibold text-gray-900 mb-4">Information</h3>
-              <ul className="space-y-3">
-                <li className="flex items-center">
-                  <span className="w-24 font-medium text-gray-900">Name:</span>
-                  <span className="text-gray-600">Derek</span>
-                </li>
-                <li className="flex items-center">
-                  <span className="w-24 font-medium text-gray-900">Gmail:</span>
-                  <span className="text-gray-600">Liudongjie1119@gmail.com</span>
-                </li>
-                <li className="flex items-center">
-                  <span className="w-24 font-medium text-gray-900">City:</span>
-                  <span className="text-gray-600">Calgary, Canada</span>
-                </li>
-                <li className="flex items-center">
-                  <span className="w-24 font-medium text-gray-900">Education:</span>
-                  <span className="text-gray-600">Bachelor of Computer Science</span>
-               
-                </li>
-              </ul>
-            </div> */}
+           
           </div>
         </div>
       </div>
